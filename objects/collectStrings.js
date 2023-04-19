97964e1ffa205e2b51e8358c645138a8055f2482
BillStephens2022 <two4onebill@yahoo.com>
2023-04-19 15:49:26 -0400

    added 3 object recursion functions, collectStrings, nestedEvenSum, and stringifyNumbers

---
+++ objects/collectStrings.js
@@ -0,0 +1,42 @@
+// Write a function called collectStrings which accepts an object and 
+// returns an array of all the values in the object that have a typeof string
+
+// uses helper method recursion
+
+function collectStrings(obj) {
+    let stringsArr = [];
+ 
+    function gatherStrings(o) {
+        for(var key in o) {
+            if(typeof o[key] === 'string') {
+                stringsArr.push(o[key]);
+            }
+            else if(typeof o[key] === 'object') {
+                return gatherStrings(o[key]);
+            }
+        }
+    }
+ 
+    gatherStrings(obj);
+ 
+    return stringsArr;
+}
+
+
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+console.log(collectStrings(obj)); // ["foo", "bar", "baz"])
