9a09d9ec996f11614fea83cea5c3b4f8fe1feb0a
BillStephens2022 <two4onebill@yahoo.com>
2023-04-19 12:41:23 -0400

    added recursive countDown function

---
+++ math/countDown.js
@@ -0,0 +1,13 @@
+// recursive countdown function
+
+function countDown(num) {
+  if (num <=0 ) {
+    console.log('All done!');
+    return;
+  }
+  console.log(num);
+  num--;
+  countDown(num);
+}
+
+countDown(10);
