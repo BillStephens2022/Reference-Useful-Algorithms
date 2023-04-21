291f14123cdb24db974302254a7e4e55bba0ad88
BillStephens2022 <two4onebill@yahoo.com>
2023-04-21 16:44:28 -0400

    added pop method to SinglyLinkedList class

--- dataStructures/singlyLinkedList.js
+++ dataStructures/singlyLinkedList.js
@@ -23,6 +23,23 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+    pop() {
+      if(!this.head) return undefined;
+      let current = this.head;
+      let newTail = current;
+      while(current.next) {
+        newTail = current;
+        current = current.next;
+      }
+      this.tail = newTail;
+      this.tail.next = null;  // severs the old tail
+      this.length--;
+      if (this.length === 0) {
+        this.head = null;
+        this.tail = null;
+      }
+      return current;
+    }
     traverse() {
       let current = this.head;
       while(current) {
@@ -44,3 +61,9 @@ list.push("NICE2MEETU");
 
 console.log(list);
 list.traverse();
+list.pop();
+console.log(list);
+list.traverse();
+list.pop();
+list.pop();
+console.log(list);
