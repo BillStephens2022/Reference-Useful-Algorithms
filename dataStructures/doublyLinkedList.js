8ca8d4dabbecadb378871b38e210c218c15b5e26
BillStephens2022 <two4onebill@yahoo.com>
2023-04-24 17:09:58 -0400

    added pop method to DoublyLinkedList class

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -25,6 +25,20 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (this.length === 0) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+        this.head = null;
+        this.tail = null;
+    } else {
+        this.tail = poppedNode.prev;
+        this.tail.next = null;
+        poppedNode.prev = null; 
+    }
+    this.length--;
+    return poppedNode;
+  }
 }
 
 
