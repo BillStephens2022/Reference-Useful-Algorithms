b2a7062014957187c5d4c1b04275275b8994b4d6
BillStephens2022 <two4onebill@yahoo.com>
2023-04-24 11:40:14 -0400

    added unshift, get, set, and insert methods to the singlyLinkedList class

--- dataStructures/singlyLinkedList.js
+++ dataStructures/singlyLinkedList.js
@@ -11,6 +11,7 @@ class SinglyLinkedList{
         this.tail = null;
         this.length = 0;
     }
+    // adds a value to the end of a list
     push(val) {
         let newNode = new Node(val);
         if (!this.head) {
@@ -23,6 +24,7 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+    // removes the last item of the list
     pop() {
       if(!this.head) return undefined;
       let current = this.head;
@@ -40,6 +42,7 @@ class SinglyLinkedList{
       }
       return current;
     }
+    // removes the item at the beginning of the list
     shift() {
       if (!this.head) return undefined;
       let currentHead = this.head;
@@ -50,6 +53,53 @@ class SinglyLinkedList{
       }
       return currentHead;
     }
+    // adds an item to the beginning of the list
+    unshift(val) {
+      let newNode = new Node(val);
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = this.head;
+      } else {
+        newNode.next = this.head;
+        this.head = newNode;
+      }    
+      this.length++;
+      return this;
+    }
+    // gets an item at a specific index
+    get(index) {
+      if (index < 0 || index >= this.length) return null;
+      let counter = 0;
+      let current = this.head;
+      while(counter !== index) {
+        current = current.next;
+        counter++;
+      }
+      return current;
+    }
+    // updates the value at an index with a new value.
+    set(index, val) {
+      let foundNode = this.get(index);
+      if (foundNode) {
+        foundNode.val = val;
+        return true;
+      }
+      return false;
+    }
+    // adds a node to a list and inserts it into a specific position
+    insert(index, val) {
+      if (index < 0 || index > this.length) return false;     
+      if (index === this.length) return !!this.push(val);   //the "!!" returns true, which means the push works
+      if (index === 0) return !!this.unshift(val);  //the "!!" returns true, which means the unshift works
+      let newNode = new Node(val);
+      let prev = this.get(index - 1);
+      var temp = prev.next;
+      prev.next = newNode;
+      newNode.next = temp;
+      this.length++;
+      return true;
+    }
+    // traverses the entire list and console logs each item in the list.
     traverse() {
       let current = this.head;
       while(current) {
@@ -58,11 +108,14 @@ class SinglyLinkedList{
       }
     }
 }
-let first = new Node("Hi");
-first.next = new Node("there");
-first.next.next = new Node("how");
-first.next.next.next = new Node("are");
-first.next.next.next.next = new Node("you");
+
+// shows how you can build a singly linked list by just adding 1 node at a time (which is inefficient with all the dot nexts). 
+// better way is to define a class and methods as done above.).
+// let first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
 
 let list = new SinglyLinkedList();
 list.push("HELLO");
@@ -70,10 +123,18 @@ list.push("GOODBYE");
 list.push("NICE2MEETU");
 list.shift();
 console.log(list);
-list.traverse();
+
 // list.pop();
 // console.log(list);
 // list.traverse();
 // list.pop();
 // list.pop();
 // console.log(list);
+list.unshift("SECOND");
+list.unshift("FIRST");
+list.traverse();
+console.log(list.get(3));
+list.set(2, "THIRD");
+list.insert(2, "TWO AND ONE HALF");
+list.insert(0, "ZERO");
+list.traverse();
