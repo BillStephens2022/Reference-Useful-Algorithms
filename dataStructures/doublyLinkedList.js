795f5c2e57b5097f70222bbfa90131bb8cb246ea
BillStephens2022 <two4onebill@yahoo.com>
2023-04-24 12:49:02 -0400

    added push method to DoublyLinkedList class

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -1,5 +1,5 @@
 class Node {
-    constructor(val){
+    constructor(val) {
         this.val = val;
         this.next = null;
         this.prev = null;
@@ -12,5 +12,24 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+        this.tail.next = newNode;
+        newNode.prev = this.tail;
+        this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
+
+let list = new DoublyLinkedList();
+list.push(99);
+console.log(list);
+list.push(100);
+console.log(list);
