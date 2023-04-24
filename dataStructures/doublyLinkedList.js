4900568a2efc02670d36658a1780a31ea01a070b
BillStephens2022 <two4onebill@yahoo.com>
2023-04-24 17:22:57 -0400

    added shift method to DoublyLinkedList class

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -1,9 +1,9 @@
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-        this.prev = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
 }
 
 class DoublyLinkedList {
@@ -12,38 +12,59 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  // add a node to the end of the list
   push(val) {
     let newNode = new Node(val);
     if (this.length === 0) {
       this.head = newNode;
       this.tail = newNode;
     } else {
-        this.tail.next = newNode;
-        newNode.prev = this.tail;
-        this.tail = newNode;
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
     }
     this.length++;
     return this;
   }
+  // remove the node at the end of the list
   pop() {
     if (this.length === 0) return undefined;
     let poppedNode = this.tail;
     if (this.length === 1) {
-        this.head = null;
-        this.tail = null;
+      this.head = null;
+      this.tail = null;
     } else {
-        this.tail = poppedNode.prev;
-        this.tail.next = null;
-        poppedNode.prev = null; 
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
     }
     this.length--;
     return poppedNode;
   }
+  // remove a node from the beginning of the list
+  shift() {
+    if (this.length === 0) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
 }
-
 
 let list = new DoublyLinkedList();
 list.push(99);
-console.log(list);
 list.push(100);
+list.push(150);
+list.push(200);
+console.log(list);
+// list.pop();
+// console.log(list);
+list.shift();
 console.log(list);
