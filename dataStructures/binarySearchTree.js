cf06a3c9c26a632709b56ad955c6b22fdb6f893b
BillStephens2022 <two4onebill@yahoo.com>
2023-04-25 14:21:28 -0400

    added insert method for BinarySearchTree class

--- dataStructures/binarySearchTree.js
+++ dataStructures/binarySearchTree.js
@@ -10,12 +10,39 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return undefined;
+      if (val < current.val) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
 
 let tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.left = new Node(7);
-tree.root.right = new Node(15);
-tree.root.left.right = new Node(9);
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
 
 console.log(tree);
