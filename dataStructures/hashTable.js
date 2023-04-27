f9a519bedd2178263c1a253147ea78c661f34b1f
BillStephens2022 <two4onebill@yahoo.com>
2023-04-27 16:13:01 -0400

    added get method for HashTable class

--- dataStructures/hashTable.js
+++ dataStructures/hashTable.js
@@ -1,23 +1,49 @@
 class HashTable {
-    constructor(size=53) {
-        this.keyMap = new Array(size);
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
     }
-    _hash(key) {
-        let total = 0;
-        let WEIRD_PRIME = 31;
-        for (let i = 0; i < Math.min(key.length, 100); i++) {
-            let char = key[i];
-            let value = char.charCodeAt(0) - 96;
-            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    return total;
+  }
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+    return index;
+  }
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if(this.keyMap[index][i][0] === key) {
+            return this.keyMap[index][i][1];
         }
-        return total;
+      }
     }
-    set(key, value) {
-        let index = this._hash(key);
-        if (!this.keyMap[index]) {
-            this.keyMap[index] = [];
-        }
-        this.keyMap[index].push([key, value]);
-        return index;
-    }
+    return undefined;
+  }
+  
 }
+
+let ht = new HashTable(17);
+ht.set("maroon", "#800000");
+ht.set("yellow", "#FFFF00");
+ht.set("olive", "#808000");
+ht.set("salmon", "#FA8072");
+ht.set("lightcoral", "#F08080");
+ht.set("mediumvioletred", "#C71585");
+ht.set("plum", "#DDA0DD");
+
+console.log(ht.keyMap);
+console.log(ht.get("yellow")); // #FFFF00
+console.log(ht.get("maroon")); // #800000
+console.log(ht.get("mediumvioletred")); // #C71585
