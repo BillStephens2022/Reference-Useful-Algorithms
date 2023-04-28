c85a3bcb76b278a37c659aec164888963ef0f0cd
BillStephens2022 <two4onebill@yahoo.com>
2023-04-28 11:54:37 -0400

    added depthFirst traversal methods to Graph class

--- dataStructures/graph.js
+++ dataStructures/graph.js
@@ -26,23 +26,62 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+    
+    (function dfs(vertex) {
+      if(!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+            return dfs(neighbor);
+        }
+      });
+    })(start);
+    return result;
+  }
+  depthFirstIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+
+    return result;
+  }
 }
 
 
 let g = new Graph();
-g.addVertex("Dallas");
-g.addVertex("Tokyo");
-g.addVertex("Aspen");
-g.addVertex("New York");
-g.addVertex("San Francisco");
-g.addEdge("Dallas", "Tokyo");
-g.addEdge("Aspen", "Dallas");
-g.addEdge("Aspen", "San Francisco");
-g.addEdge("New York", "San Francisco");
-g.addEdge("Dallas", "New York");
-g.addEdge("New York", "Tokyo");
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
 console.log(g.adjacencyList);
-g.removeEdge("Dallas", "Tokyo");
-console.log(g.adjacencyList);
-g.removeVertex("Aspen");
-console.log(g.adjacencyList);
+console.log(g.depthFirstRecursive("A"));
+console.log(g.depthFirstIterative("A"));
+
