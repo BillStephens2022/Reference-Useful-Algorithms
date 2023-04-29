8488be1e3037fa42017c0486ae32c1519fc13ba0
BillStephens2022 <two4onebill@yahoo.com>
2023-04-29 09:19:35 -0400

    added optimized fibonacci function using tabulation bottoms up approach

--- fibonacciFunctions/fibonacci.js
+++ fibonacciFunctions/fibonacci.js
@@ -1,9 +1,12 @@
-// recursive fibonacci - not optimal (slow when working with larger #'s)
+// recursive fibonacci - not optimal (slow when working with larger #'s and will crash if num too large)
 
 function fibRecursive(num) {
     if (num <= 2) return 1;
     return fibRecursive(num-1) + fibRecursive(num-2);
 }
+
+console.log("fibRecursive: ", fibRecursive(5)); // 5
+console.log("fibRecursive: ", fibRecursive(10)); // 55
 
 // 'Memoized' verstion, eliminates recursive calls of fib(n)'s already solved.  i.e. if doing fib(10),
 // it won't keep resolving fib(3) or fib(4) since it has already solved these and stored in the 
@@ -15,5 +18,21 @@ function fibMemo(num, memo=[undefined, 1, 1]) {
   return result;
 }
 
-console.log(fibMemo(5)); // 5
-console.log(fibMemo(10)); // 55
+console.log("fibMemo: ", fibMemo(5)); // 5
+console.log("fibMemo: ", fibMemo(45)); // 1134903170  
+//NOTE:  fib of 45 would be VERY slow to calc using the recursive function
+
+
+// 'bottom-up' approach.  as it solves smaller nums, it saves in memory and builds up the array
+// as it approaches num.  This is most optimized and can do larger nums quickly and without crashing.
+function fibTable(num) {
+    if (num <= 2) return 1;
+    let fibNums = [0, 1, 1];
+    for (let i = 3; i <= num; i++) {
+        fibNums[i] = fibNums [i-1] + fibNums[i-2];
+    }
+    return fibNums[num];
+  }
+
+console.log("fibTable: ", fibTable(5)); // 5
+console.log("fibTable: ", fibTable(60)); // 1548008755920
